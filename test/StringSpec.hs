63c6fef854ab954a2c4adf10a6e6d6ecab159693
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2018-01-11 13:45:46 +0000

    Clean: useless extensions

--- test/StringSpec.hs
+++ test/StringSpec.hs
@@ -17,10 +17,6 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE Rank2Types #-}
-
 module StringSpec where
 
 import           Control.Lens
